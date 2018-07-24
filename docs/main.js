fed38dd1a2a8b977187587c06d585dad1f7674b5
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-24 16:55:38 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,24 @@
 const myId = (new MediaStream).id;
 console.log(`myId:${myId}`);
 let stream = null;
+function appendVideo(stream) {
+    const video = document.createElement('video');
+    video.srcObject = stream;
+    document.body.appendChild(video);
+    video.play();
+}
 navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     console.log(`streamId:${stream.id}`);
-    const localView = document.createElement('video');
-    localView.srcObject = stream;
-    document.body.appendChild(localView);
-    localView.play();
+    appendVideo(stream);
     const peer = new Peer(myId, {
         key: '01099bd8-1083-4c33-ba9b-564a1377e901'
     });
     peer.on('open', id => {
         myIdDisp.textContent = id;
-        const room = peer.joinRoom('hoge_fuga_piyo', { mode: 'sfu', stream });
+        const room = peer.joinRoom('hoge_fuga_piyo_sfu', { mode: 'sfu', stream });
         room.on('stream', stream => {
             console.log(`room on stream peerId:${stream.peerId}`);
-            const remoteView = document.createElement('video');
-            remoteView.srcObject = stream;
-            document.body.appendChild(remoteView);
-            remoteView.play();
+            appendVideo(stream);
         });
     });
 }).catch(err => {
