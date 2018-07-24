533a9e003f64e46f6e83c1bc8828e9984d5687d2
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-24 16:45:35 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,10 @@ console.log(`myId:${myId}`);
 let stream = null;
 navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
     console.log(`streamId:${stream.id}`);
+    const localView = document.createElement('video');
     localView.srcObject = stream;
+    document.body.appendChild(localView);
+    localView.play();
     const peer = new Peer(myId, {
         key: '01099bd8-1083-4c33-ba9b-564a1377e901'
     });
@@ -12,7 +15,10 @@ navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
         const room = peer.joinRoom('hoge_fuga_piyo', { mode: 'sfu', stream });
         room.on('stream', stream => {
             console.log(`room on stream peerId:${stream.peerId}`);
+            const remoteView = document.createElement('video');
             remoteView.srcObject = stream;
+            document.body.appendChild(remoteView);
+            remoteView.play();
         });
     });
 }).catch(err => {
