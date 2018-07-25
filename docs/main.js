f3e0dac33ec1ce7296eca96dfc18d83e6d845275
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-25 14:14:44 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -15,7 +15,7 @@ navigator.mediaDevices.getUserMedia(constraints).then(stream => {
     console.log(`streamId:${stream.id}`);
     appendVideo(stream);
     const peer = new Peer(myId, {
-        key: '4a39738a-ba85-4ab5-9e6c-f0bc742042a4' // 5分間映像のみ送受信 (3人目接続しすぐに3人目を切断)
+        key: '9e6003cb-dcd5-43bb-93e5-0cac7107c5c9' // 5分間映像のみ送受信 (2人接続して、2人目が1人目の映像が表示されていない状態)
     });
     peer.on('open', id => {
         myIdDisp.textContent = id;
